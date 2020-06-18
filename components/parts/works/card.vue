d3901898f962588ce2c3b6391f316dff5f2d84b3
Junya Oiwa <0920oiwa@gmail.com>
2020-06-19 00:41:49 +0900

    [add]works section

---
+++ components/parts/works/card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card-wrapper w-screen h-full flex-shrink-0 flex justify-center items-center">
+    <div class="card bg-white">
+      <p>あいうえお～</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card-wrapper {
+  max-width: 640px;
+}
+
+.card {
+  width: 90%;
+  height: 100%;
+}
+</style>
--- components/sections/works.vue
+++ components/sections/works.vue
@@ -1,13 +1,41 @@
 <template>
-  <section id="works" class="h-screen w-full bg-cover bg-center bg-gray-800"></section>
+  <section id="works" class="h-screen w-full bg-gray-800 flex items-center justify-center">
+    <div
+      class="overflow-hidden w-full flex justify-center items-center display-none"
+      style="height: 80%; min-height: 500px;"
+    >
+      <div
+        id="carousel"
+        class="w-full sm:w-9/12 h-full overflow-x-scroll flex whitespace-no-wrap"
+        v-dragscroll="true"
+      >
+        <card />
+        <card />
+        <card />
+        <card />
+        <card />
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import card from '~/components/parts/works/card.vue';
+
+export default {
+  components: {
+    card,
+  },
+};
 </script>
 
 <style>
 #works {
   scroll-snap-align: start;
+}
+
+#carousel::-webkit-scrollbar {
+  display: none;
+  -webkit-appearance: none;
 }
 </style>
